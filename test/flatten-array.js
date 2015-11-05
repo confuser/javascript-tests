9694cd24e67bfe59910db538e9cfee7e757f0839
James Mortemore <jamesmortemore@gmail.com>
2015-11-05 21:03:18 +0000

    Added recursive flatten array function with maxDepth to prevent blocking the event loop ununnecessary

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,33 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    arr = flatten(arr, 4);
+
     expect(arr).toEqual(expected);
   });
 });
+
+/*
+ * Recursion in node is dangerous as it blocks the process for an unknown
+ * amount of time, therefore maxDepth ensures too much nesting doesn't hang
+ * the process. Switch to a defered async version if required.
+ */
+function flatten(elements, maxDepth) {
+  var flattened = [];
+  var depth = 0;
+
+  function iterate(array) {
+    // Change to for(var i = 0...) loop if bottleneck
+    array.forEach(function (element) {
+      if (Array.isArray(element) && depth !== maxDepth) return iterate(element);
+
+      flattened.push(element);
+    });
+
+    depth++;
+  }
+
+  iterate(elements);
+
+  return flattened.sort();
+}
