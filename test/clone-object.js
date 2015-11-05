60fcea4361e2a59108b764279b8d869e39e5c4a1
James Mortemore <jamesmortemore@gmail.com>
2015-11-05 21:02:24 +0000

    Added crude object clone
    JSON.parse and JSON.stringify block the event loop in node, best to
    avoid using it for basic object cloning

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,9 +1,33 @@
 describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+        obj = clone(expected);
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
 });
+
+/*
+ * Crude cloning of an object, uses assign in ES6 compatible environments
+ * Otherwise defaults to a simple Object enumerable properties copy
+ * Does not support cyclic dependencies, use alternative such as
+ * lodash/underscore deepClone
+ */
+function clone(obj) {
+  if (!obj || typeof obj !== 'object') return obj;
+
+  // Check if ES6 compatible environment
+  if (Object.assign) return Object.assign({}, obj);
+
+  var cloned = Object(obj.constructor.prototype);
+
+  for (var prop in obj) {
+    var desc = Object.getOwnPropertyDescriptor(obj, prop);
+
+    if (desc && desc.enumerable) cloned[prop] = obj[prop];
+  }
+
+  return cloned;
+
+}
